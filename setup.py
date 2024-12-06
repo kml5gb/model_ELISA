4399feed90d6b9f6069546030aebedfbea666a82
kml5gb <kml5gb@uw.edu>
2024-12-06 12:16:47 -0800

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model_ELISA',  
-    version='0.1.2',  
+    version='0.1.3',  
     packages=find_packages(),  
     install_requires=['numpy', 'matplotlib', 'tellurium'],  # List of dependencies if any
     description='A GUI to simulate the direct ELISA model',  
